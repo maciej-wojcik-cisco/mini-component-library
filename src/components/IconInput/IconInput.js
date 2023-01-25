80f26eea83baa578e7192bb05e40a85aebe0e878
maciej-wojcik-cisco <macwojci@cisco.com>
2023-01-25 13:29:31 +0100

    Input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import { COLORS } from '../../constants';
+
 
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
@@ -11,9 +11,61 @@ const IconInput = ({
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
-  return 'TODO';
+
+  const iconSize = size === 'small' ? 16 : 24;
+  const fontSize = size === 'small' ? 1 : 1.5;
+  const height = size === 'small' ? 1.5 : 2;
+
+  return (
+    <Container>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconContainer style={{'--size': iconSize +'px'}}>
+        <Icon id={icon} size={iconSize}></Icon>
+      </IconContainer>
+      <NativeInput {...delegated} style={{
+        '--width': width + 'px', 
+        '--font-size': fontSize +'rem',
+        '--height': height +'rem'
+      }}></NativeInput>
+    </Container>
+  );
 };
+
+const Container = styled.label`
+  position: relative;
+  display: block;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconContainer = styled.div`
+  position: absolute;
+  top:0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
+
+const NativeInput = styled.input`
+  width: var(--width);
+  height: var(--height);
+  font-size: var(--font-size);
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
+  padding-left: 24px;
+  font-weight: 700;
+  outline-offset: 3px;
+  color: inherit;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
 
 export default IconInput;
